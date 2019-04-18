878a2975b217ec6b10d51832cdd46e52c8f5b0b1
Keval Shah <Keval0001@gmail.com>
2019-04-18 09:04:41 -0600

    First react component and app

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,35 +1,12 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-=======
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
->>>>>>> branch11
+import Dashboard from "./components/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-<<<<<<< HEAD
-            Edit <code>src/App.js</code> ss d to reload.
-=======
-            Edit <code>src/App.js</code> and save to reload.
->>>>>>> branch11
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Dashboard />
       </div>
     );
   }
