7ed76fc2b4c70bb8ad583e07f66daf2c44689559
Keval Shah <Keval0001@gmail.com>
2019-05-16 10:29:49 -0600

    47-51 Backlog relationship, persisting project task Project Backlog

--- PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/domain/Project.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Project {
@@ -50,6 +51,7 @@ public class Project {
 
 	//Cascade is ownig side
 	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+	@JsonIgnore
 	private Backlog backlog;
 	
 	public Project() {
